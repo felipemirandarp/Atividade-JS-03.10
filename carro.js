6c8dd0be19d091a52e534c0fbb1f02a552df5aeb
Felipe Miranda <felipemirandaresendepinheiro@gmail.com>
2024-10-04 15:53:23 -0300

    Add files via upload

--- carro.js
+++ carro.js
@@ -5,13 +5,40 @@ let carro = {
     valor: "",
 }
 
-let carro2 = {
-    modelo: "",
-    marca: "",
-    ano: "",
-    valor: "",
-}
+let carros = []
+var op
 
-    carro.modelo = prompt("Digite o modelo do carro: ")
-    carro.marca = prompt("Digite a marca do carro: ")
-    carro.ano = Number(prompt("Digite o ano do carro: "))
+    do{
+        carro.modelo = prompt("Digite o modelo do carro: ")
+        carro.marca = prompt("Digite a marca do carro: ")
+        carro.ano = Number(prompt("Digite o ano do carro: "))
+        carro.valor = parseFloat(prompt("Digite o valor do carro: "))
+
+        carros.push (carro)
+
+        op = confirm("Você deseja continuar? ")
+
+    }while(op)
+
+        for(let i = 0; i < carros.length; i++){
+
+            let maiorvalor = 0 
+            var indicemaiorvalor = 0 
+
+            if( i == 0 ){
+                maiorvalor = carros[i].valor
+                indicemaiorvalor  = i 
+                
+            }else{
+                if( carros[i].valor > maiorvalor ){
+                    maiorvalor = carros[i].valor
+                    indicemaiorvalor  = i 
+                }
+            }   
+
+
+
+        }
+        
+
+        console.log(carros[indicemaiorvalor])
